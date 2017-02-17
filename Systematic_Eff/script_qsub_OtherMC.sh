c51a2119e1bbde97211e1a7a588c4c09b4412757
Kyoengpil Lee <kplee@cern.ch>
2017-02-17 10:45:38 +0100

    update qsub script

--- Systematic_Eff/script_qsub_OtherMC.sh
+++ Systematic_Eff/script_qsub_OtherMC.sh
@@ -4,20 +4,20 @@ cwd2=$(pwd)
 
 # cp /home/kplee/Physics/script/Split_BatchJobs_80X.py ./
 
-# python Split_BatchJobs_80X.py --code ProdHist_Systematic_Eff.cxx --sample WWTo2L2Nu --njob 1 --lumi 35867.060 --isMC 1
-# cd ./WWTo2L2Nu; source qsub_all.sh
+python Split_BatchJobs_80X.py --code ProdHist_Systematic_Eff.cxx --sample WWTo2L2Nu --njob 1 --lumi 35867.060 --isMC 1
+cd ./WWTo2L2Nu; source qsub_all.sh
 
-# cd ${cwd2}
-# python Split_BatchJobs_80X.py --code ProdHist_Systematic_Eff.cxx --sample WZ --njob 1 --lumi 35867.060 --isMC 1
-# cd ./WZ; source qsub_all.sh
+cd ${cwd2}
+python Split_BatchJobs_80X.py --code ProdHist_Systematic_Eff.cxx --sample WZ --njob 1 --lumi 35867.060 --isMC 1
+cd ./WZ; source qsub_all.sh
 
-# cd ${cwd2}
-# python Split_BatchJobs_80X.py --code ProdHist_Systematic_Eff.cxx --sample ZZ --njob 1 --lumi 35867.060 --isMC 1
-# cd ./ZZ; source qsub_all.sh
+cd ${cwd2}
+python Split_BatchJobs_80X.py --code ProdHist_Systematic_Eff.cxx --sample ZZ --njob 1 --lumi 35867.060 --isMC 1
+cd ./ZZ; source qsub_all.sh
 
-# cd ${cwd2}
-# python Split_BatchJobs_80X.py --code ProdHist_Systematic_Eff.cxx --sample ttbarTo2L2Nu --njob 10 --lumi 35867.060 --isMC 1
-# cd ./ttbarTo2L2Nu; source qsub_all.sh
+cd ${cwd2}
+python Split_BatchJobs_80X.py --code ProdHist_Systematic_Eff.cxx --sample ttbarTo2L2Nu --njob 10 --lumi 35867.060 --isMC 1
+cd ./ttbarTo2L2Nu; source qsub_all.sh
 
 cd ${cwd2}
 python Split_BatchJobs_80X.py --code ProdHist_Systematic_Eff.cxx --sample tW --njob 2 --lumi 35867.060 --isMC 1
