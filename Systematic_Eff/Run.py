31ca20c40ad13902c5bb9a9bebec05f4041e3355
Kyoengpil Lee <kplee@cern.ch>
2017-02-17 12:57:04 +0100

    add automatic qsub and hadd part

--- Systematic_Eff/Run.py
+++ Systematic_Eff/Run.py
@@ -13,6 +13,55 @@ def instruction():
     print "   --queue queueName                      queue name (default: fastq)"
     sys.exit()
 
+def Add_DYPowheg( _List_Samples ):
+	_List_Samples.append( ["DYPowheg_M50to120", 1, 5] )
+	_List_Samples.append( ["DYPowheg_M120to200", 1, 1] )
+	_List_Samples.append( ["DYPowheg_M200to400", 1, 1] )
+	_List_Samples.append( ["DYPowheg_M400to800", 1, 1] )
+	_List_Samples.append( ["DYPowheg_M800to1400", 1, 1] )
+	_List_Samples.append( ["DYPowheg_M1400to2300", 1, 1] )
+	_List_Samples.append( ["DYPowheg_M2300to3500", 1, 1] )
+	_List_Samples.append( ["DYPowheg_M3500to4500", 1, 1] )
+	_List_Samples.append( ["DYPowheg_M4500to6000", 1, 1] )
+	_List_Samples.append( ["DYPowheg_M6000toInf", 1, 1] )
+
+def MakeScript_Sub(_OutDir, _List_Samples):
+	f = open("./script_qsub_ALL.sh", "w")
+	f.write( "#!bin/bash\n" )
+	f.write( "cwd=$(pwd)\n\n" )
+	f.write( "cd %s" % (_OutDir) )
+	for Samples in _List_Samples:
+		SampleName = Samples[0]
+		f.write( "cd %s; source qsub_all.sh" % (SampleName) )
+		f.write( "\n" )
+
+	f.write( "\n\n" )
+
+	f.write( 'cd ${cwd}\n' )
+	f.write( 'echo "finished"\n' )
+	f.close()
+
+	print "source script_qsub_ALL.sh" % (self.Sample)
+
+def MakeScript_GetOutput(_OutDir, _List_Samples):
+	f = open("./script_GetOutput.sh", "w")
+	f.write( "#!bin/bash\n" )
+	f.write( "cwd=$(pwd)\n\n" )
+	f.write( "cd %s" % (_OutDir) )
+
+	for Samples in _List_Samples:
+		SampleName = Samples[0]
+		f.write( "cd %s; source hadd_all.sh\n" % (SampleName) )
+		f.write( "cp *.root ${cwd}\n" )
+		f.write( "\n" )
+
+	f.write( "\n\n" )
+
+	f.write( 'cd ${cwd}\n' )
+	f.write( 'echo "finished"\n' )
+	f.close()
+
+# -- main -- #
 # Parse arguments
 if len(sys.argv) < 2: instruction()
 try:
@@ -22,20 +71,6 @@ except:
     print "!!! Error parsing arguments"
     instruction()
 
-def Add_DYPowheg( _List_Samples ):
-	List_Samples.append( ["DYPowheg_M50to120", 1, 5] )
-	List_Samples.append( ["DYPowheg_M120to200", 1, 1] )
-	List_Samples.append( ["DYPowheg_M200to400", 1, 1] )
-	List_Samples.append( ["DYPowheg_M400to800", 1, 1] )
-	List_Samples.append( ["DYPowheg_M800to1400", 1, 1] )
-	List_Samples.append( ["DYPowheg_M1400to2300", 1, 1] )
-	List_Samples.append( ["DYPowheg_M2300to3500", 1, 1] )
-	List_Samples.append( ["DYPowheg_M3500to4500", 1, 1] )
-	List_Samples.append( ["DYPowheg_M4500to6000", 1, 1] )
-	List_Samples.append( ["DYPowheg_M6000toInf", 1, 1] )
-
-
-# -- main -- #
 # -- SampleName, isMC, nJobs -- #
 List_Samples = []
 # List_Samples.append( ["Data", 0, 20] )
@@ -54,6 +89,12 @@ os.mkdir( OutDir )
 
 # -- convert path for code to absolute path -- #
 CodeAbsPath = os.path.abspath( MainOpts['--code'] )
+
+# -- make script -- #
+MakeScript_Sub( OutDir, List_Samples )
+MakeScript_GetOutput( OutDir, List_Samples )
+
+
 
 for Samples in List_Samples:
 	SampleName = Samples[0]
