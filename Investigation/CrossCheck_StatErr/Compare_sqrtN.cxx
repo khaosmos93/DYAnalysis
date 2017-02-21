ff9beea8d228ec717be4a6a67380770def5dd143
Kyoengpil Lee <kplee@cern.ch>
2017-02-22 00:44:55 +0100

    add Andrius' result

--- Investigation/CrossCheck_StatErr/Compare_sqrtN.cxx
+++ Investigation/CrossCheck_StatErr/Compare_sqrtN.cxx
@@ -19,8 +19,11 @@ void Compare_sqrtN()
 	TH1D* h_DiffXSec_KP = Get_Hist( FileName_KP, "h_DiffXsec_FSRCorr" );
 	TH1D* h_RelStatUnc_KP = Get_Hist( FileName_KP, "h_RelStatUnc_Percent" ); h_RelStatUnc_KP->Scale( 0.01 );
 
+	TString FileName_Andrius = GetBasePath() + "Include/Results_ROOTFiles_76X/dymm_study_yieldErrPropagation_5000.root";
+	TH1D* h_DiffXSec_Andrius = Get_Hist( FileName_Andrius, "h1__cs" );
+	TH1D* h_RelStatUnc_Andrius = Extract_RelUnc( h_DiffXSec_Andrius );
+
 	TH1D* h_1OverSqrtN = (TH1D*)h_RelStatUnc_KP->Clone("h_1OverSqrtN");
-	// TH1D* h_AbsStatUnc_KP = (TH1D*)h_RelStatUnc_KP->Clone("h_AbsStatUnc_KP");
 	for(Int_t i=0; i<nMassBin; i++)
 	{
 		Int_t i_bin = i+1;
@@ -33,13 +36,14 @@ void Compare_sqrtN()
 
 		Double_t DiffXSec_KP = h_DiffXSec_KP->GetBinContent(i_bin);
 		Double_t RelStatUnc_KP = h_RelStatUnc_KP->GetBinContent(i_bin);
+		Double_t RelStatUnc_Andrius = h_RelStatUnc_Andrius->GetBinContent(i_bin);
 		// Double_t AbsStatUnc_KP = DiffXSec_KP * RelStatUnc_KP;
 
 		// h_AbsStatUnc_KP->SetBinContent(i_bin, AbsStatUnc_KP );
 		// h_AbsStatUnc_KP->SetBinError(i_bin, 0);
 
-		printf("[%02d bin: %5.1lf < M < %5.1lf] (N, 1/SqrtN, RelStatUnc (KP)) = (%10.0lf, %9.3lf, %9.3lf)\n",
-			i_bin, MassBinEdges[i], MassBinEdges[i+1], nYield, OneOverSqrtN*100, RelStatUnc_KP*100 );
+		printf("[%02d bin: %5.1lf < M < %5.1lf] (N, 1/SqrtN, RelStatUnc (KP)) = (%10.0lf, %9.3lf, %9.3lf, %9.3lf)\n",
+			i_bin, MassBinEdges[i], MassBinEdges[i+1], nYield, OneOverSqrtN*100, RelStatUnc_KP*100, RelStatUnc_Andrius*100 );
 	}
 
 	HistInfo *Hist_1OverSqrtN = new HistInfo( kBlack, "1 / #sqrt{N^{obs}}");
@@ -51,14 +55,20 @@ void Compare_sqrtN()
 	Hist_KP->Set();
 	Hist_KP->Calc_RatioHist_Denominator( Hist_1OverSqrtN->h );
 
+	HistInfo *Hist_Andrius = new HistInfo( kGreen+2, "Stat. by Andrius" );
+	Hist_Andrius->Set_Histogram( h_RelStatUnc_Andrius );
+	Hist_Andrius->Set();
+	Hist_Andrius->Calc_RatioHist_Denominator( Hist_1OverSqrtN->h );
+
 	TCanvas *c; TPad *TopPad; TPad *BottomPad;
-	SetCanvas_Ratio( c, "Compare_AbsStatUnc", TopPad, BottomPad, 1, 1 );
+	SetCanvas_Ratio( c, "Compare_RelStatUnc", TopPad, BottomPad, 1, 1 );
 
 	c->cd();
 	TopPad->cd();
 
 	Hist_1OverSqrtN->h->Draw("LPSAME");
 	Hist_KP->h->Draw("LPSAME");
+	Hist_Andrius->h->Draw("LPSAME");
 
 	SetHistFormat_TopPad( Hist_1OverSqrtN->h, "Relative uncertainty (Stat.)");
 
@@ -66,6 +76,7 @@ void Compare_sqrtN()
 	SetLegend( legend, 0.20, 0.75, 0.50, 0.95 );
 	legend->AddEntry( Hist_1OverSqrtN->h, Hist_1OverSqrtN->LegendName );
 	legend->AddEntry( Hist_KP->h, Hist_KP->LegendName );
+	legend->AddEntry( Hist_Andrius->h, Hist_Andrius->LegendName );
 	legend->Draw();
 
 	TLatex latex;
@@ -75,6 +86,8 @@ void Compare_sqrtN()
 	BottomPad->cd();
 
 	Hist_KP->h_ratio->Draw("LPSAME");
+	Hist_Andrius->h_ratio->Draw("LPSAME");
+
 	SetHistFormat_BottomPad( Hist_KP->h_ratio, "m (#mu#mu) [GeV]", "Unc. / black", 0.9, 2.0 );
 	Hist_KP->h_ratio->GetYaxis()->CenterTitle();
 	Hist_KP->h_ratio->GetYaxis()->SetTitleOffset(0.4);
