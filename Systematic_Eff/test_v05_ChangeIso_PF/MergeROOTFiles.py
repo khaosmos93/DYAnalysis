93b6832eafc2c0e022f5336ab9bce072900db6ff
Kyoengpil Lee <kplee@cern.ch>
2017-02-24 23:12:56 +0100

    cleaning

--- Systematic_Eff/test_v05_ChangeIso_PF/MergeROOTFiles.py
+++ Systematic_Eff/test_v05_ChangeIso_PF/MergeROOTFiles.py
@@ -5,10 +5,10 @@ if not os.path.exists("./Local"):
 
 cmd_hadd = "hadd ROOTFile_DYPowheg.root ROOTFile_DYPowheg_M*.root"
 print cmd_hadd
-# os.system( cmd_hadd )
+os.system( cmd_hadd )
 cmd_mv = "mv ROOTFile_DYPowheg_M*.root ./Local"
 print cmd_mv
-# os.system( cmd_mv )
+os.system( cmd_mv )
 
 List_ROOTFile = []
 for file in os.listdir("."):
@@ -19,13 +19,14 @@ for file in os.listdir("."):
 
 f_output = TFile("ROOTFile_Systematic_Eff.root", "RECREATE")
 
+print "Loop over list of ROOT files ..."
 for ROOTFile in List_ROOTFile:
 
 	# -- make a directory -- #
 	DirName = ROOTFile.split(".")[0].split("ROOTFile_")[0]
 	print "\tDirName = %s\n" % (DirName)
+	f_output.cd() # -- return to top directory -- #
 	f_output.mkdir( DirName )
-	f_output.cd( DirName )
 
 	f_input = TFile( ROOTFile )
 	f_input.cd()
@@ -33,10 +34,12 @@ for ROOTFile in List_ROOTFile:
 	for key in f_input.GetListOfKeys():
 		obj = key.ReadObj()
 
-		f_output.cd( DirName )
+		f_output.cd( DirName ) # -- move to sub directory -- #
 		obj.Write()
+		
 		f_input.cd()
 
 	f_input.Close()
 
+print "Merging root files are finished: Output file = ", f_output.GetName()
 f_output.Close()
