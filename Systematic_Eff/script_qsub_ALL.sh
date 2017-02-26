2dc9382c4e16848fb81cc986f6a4ab8ff03988dc
KyeongPil Lee <kplee@cern.ch>
2017-02-27 05:31:01 +0900

    Results using latest codes

--- Systematic_Eff/script_qsub_ALL.sh
+++ Systematic_Eff/script_qsub_ALL.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 cwd2=$(pwd)
 
-cd /home/kplee/Physics/ZprimeAnalysis_80X/Outputs/v20170222_040920_ProdHist_Systematic_Eff
+cd /home/kplee/Physics/ZprimeAnalysis_80X/Outputs/v20170227_024739_ProdHist_Systematic_Eff
 cd Data; source qsub_all.sh
 cd ../
 cd DYPowheg_M50to120; source qsub_all.sh
