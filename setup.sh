7ba9a413b201b1f876759a457c32c820f401ec40
Kyoengpil Lee <kplee@cern.ch>
2017-02-28 00:28:12 +0100

    add setup.sh & Calc_SumWeight code

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $KP_ANALYZER_PATH ]; then
+    echo "KP_ANALYZER_PATH is already defined: use a clean shell!"
+    return 1
+fi
+
+# -- analyzer path (do not insert "/" in the end of the path)-- #
+export KP_ANALYZER_PATH=$(pwd)
+export KP_INCLUDE_PATH=$KP_ANALYZER_PATH/Include
+export KP_ROOTFILE_PATH=$KP_INCLUDE_PATH/Results_ROOTFiles
+
+# -- root setup -- #
+export ROOT_INCLUDE_PATH=${KP_ANALYZER_PATH}:${ROOT_INCLUDE_PATH}
+
+# -- ntuple path -- #
+export KP_DATA_PATH=""
+if [ $HOSTNAME == "cms.snu.ac.kr" ]; # -- 147.47.242.42 -- # 
+then 
+	KP_DATA_PATH="/data1/kplee/DYntuple/80X"
+	
+	# -- cmssw setup (for ROOT6) -- #
+	export SCRAM_ARCH=slc6_amd64_gcc530
+	export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+	source $VO_CMS_SW_DIR/cmsset_default.sh
+
+	cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_8_0_13/src
+	eval `scramv1 runtime -sh` # -- equivalent to cmsenv -- #
+	#cmsenv # -- this doesn't work. why?? -- #
+	cd $KP_ANALYZER_PATH
+fi
+
+echo "================ environment ================"
+echo "KP_ANALYZER_PATH:" ${KP_ANALYZER_PATH}
+echo "KP_INCLUDE_PATH:" ${KP_INCLUDE_PATH}
+echo "KP_ROOTFILE_PATH:" ${KP_ROOTFILE_PATH}
+
+echo "KP_DATA_PATH:" ${KP_DATA_PATH}
+if [ -z $KP_DATA_PATH ]; then
+    echo "     [WARNING]: ntuples are not available in this machine"
+fi
+
+echo "============================================="
+echo "setup is finished. Welcome :)"
