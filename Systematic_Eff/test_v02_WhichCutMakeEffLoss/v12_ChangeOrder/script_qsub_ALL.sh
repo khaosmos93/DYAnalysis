0f7bea9f73ae6368e3f6eec47ef68e0e1955737e
KyeongPil Lee <kplee@cern.ch>
2017-03-01 15:28:49 +0900

    results

---
+++ Systematic_Eff/test_v02_WhichCutMakeEffLoss/v12_ChangeOrder/script_qsub_ALL.sh
@@ -0,0 +1,42 @@
+#!bin/bash
+cwd2=$(pwd)
+
+cd /home/kplee/Physics/ZprimeAnalysis_80X/Outputs/v20170228_162753_ProdHist_Systematic_Eff
+cd Data; source qsub_all.sh
+cd ../
+cd DYPowheg_M50to120; source qsub_all.sh
+cd ../
+cd DYPowheg_M120to200; source qsub_all.sh
+cd ../
+cd DYPowheg_M200to400; source qsub_all.sh
+cd ../
+cd DYPowheg_M400to800; source qsub_all.sh
+cd ../
+cd DYPowheg_M800to1400; source qsub_all.sh
+cd ../
+cd DYPowheg_M1400to2300; source qsub_all.sh
+cd ../
+cd DYPowheg_M2300to3500; source qsub_all.sh
+cd ../
+cd DYPowheg_M3500to4500; source qsub_all.sh
+cd ../
+cd DYPowheg_M4500to6000; source qsub_all.sh
+cd ../
+cd DYPowheg_M6000toInf; source qsub_all.sh
+cd ../
+cd WWTo2L2Nu; source qsub_all.sh
+cd ../
+cd WZ; source qsub_all.sh
+cd ../
+cd ZZ; source qsub_all.sh
+cd ../
+cd ttbarTo2L2Nu; source qsub_all.sh
+cd ../
+cd tW; source qsub_all.sh
+cd ../
+cd tbarW; source qsub_all.sh
+cd ../
+
+
+cd ${cwd2}
+echo "full submission is finished"
