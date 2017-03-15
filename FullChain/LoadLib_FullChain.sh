f4968f661f06d2a5789bc128f1d482863db0b0d6
Kyoengpil Lee <kplee@cern.ch>
2017-03-15 13:26:47 +0900

    use KP_ROOT5/6_PATH

--- FullChain/LoadLib_FullChain.sh
+++ FullChain/LoadLib_FullChain.sh
@@ -3,7 +3,7 @@
 if [ $HOME == "/Users/KyeongPil_Lee" ];
 	then
 	echo "Change ROOT version: ROOT6 -> ROOT5"
-	source /Users/KyeongPil_Lee/ROOT5/root/bin/thisroot.sh
+	source ${KP_ROOT5_PATH}
 fi
 
 root -l -b << EOF
@@ -28,5 +28,5 @@ EOF
 if [ $HOME == "/Users/KyeongPil_Lee" ];
 	then
 	echo "Return to latest ROOT version: ROOT5 -> ROOT6"
-	source /Users/KyeongPil_Lee/ROOT6/root/bin/thisroot.sh
+	source ${KP_ROOT6_PATH}
 fi
