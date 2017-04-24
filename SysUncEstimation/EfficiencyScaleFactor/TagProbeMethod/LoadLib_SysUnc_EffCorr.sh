c8c340f18ac1e816228c8c4545701105a80c429e
Kyoengpil Lee <kplee@cern.ch>
2017-04-24 20:28:38 +0900

    use cmssw

--- SysUncEstimation/EfficiencyScaleFactor/TagProbeMethod/LoadLib_SysUnc_EffCorr.sh
+++ SysUncEstimation/EfficiencyScaleFactor/TagProbeMethod/LoadLib_SysUnc_EffCorr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 echo "Change ROOT version: ROOT6 -> ROOT5"
-source ${KP_ROOT5_PATH}
+# source ${KP_ROOT5_PATH}
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc493/cms/cmssw/CMSSW_7_6_3
+eval `scramv1 runtime -sh` # -- equivalent to cmsenv -- #
 
 root -l -b << EOF
 
@@ -23,4 +24,6 @@ gSystem->Load(BasePath+"/libRooUnfold.so")
 EOF
 
 echo "Return to latest ROOT version: ROOT5 -> ROOT6"
-source ${KP_ROOT6_PATH}
+# source ${KP_ROOT6_PATH}
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_8_0_13/src
+eval `scramv1 runtime -sh` # -- equivalent to cmsenv -- #
