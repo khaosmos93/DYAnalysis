7a4084742fdae1b524c48ae88a9fbcc7aa663b6c
Kyoengpil Lee <kplee@cern.ch>
2017-04-25 00:24:23 +0900

    remove unnecessary parts

--- SysUncEstimation/EfficiencyScaleFactor/TagProbeMethod/LoadLib_SysUnc_EffCorr.sh
+++ SysUncEstimation/EfficiencyScaleFactor/TagProbeMethod/LoadLib_SysUnc_EffCorr.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-echo "Change ROOT version: ROOT6 -> ROOT5"
-# source ${KP_ROOT5_PATH}
-SCRAM_ARCH=slc6_amd64_gcc493
-cd /cvmfs/cms.cern.ch/slc6_amd64_gcc493/cms/cmssw/CMSSW_7_6_3
-eval `scramv1 runtime -sh` # -- equivalent to cmsenv -- #
 
 root -l -b << EOF
-
-TString IncludePath = gSystem->Getenv("KP_ANALYZER_PATH")
-gInterpreter->AddIncludePath( TString::Format("%s", IncludePath.Data()) )
-
-TString IncludePath2 = gSystem->Getenv("KP_ROOUNFOLD_PATH")
-gInterpreter->AddIncludePath( TString::Format("%s", IncludePath2.Data()) )
-
-.include
 
 TString BasePath = gSystem->Getenv("KP_ROOUNFOLD_PATH")
 gSystem->Load(BasePath+"/libRooUnfold.so")
@@ -24,8 +11,3 @@ gSystem->Load(BasePath+"/libRooUnfold.so")
 
 EOF
 
-echo "Return to latest ROOT version: ROOT5 -> ROOT6"
-# source ${KP_ROOT6_PATH}
-SCRAM_ARCH=slc6_amd64_gcc530
-cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_8_0_13/src
-eval `scramv1 runtime -sh` # -- equivalent to cmsenv -- #
