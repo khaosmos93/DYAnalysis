1e15a2852eec69e8dd57a7f6f7fef5d589e55338
Kyoengpil Lee <kplee@cern.ch>
2017-05-08 00:28:47 +0900

    debug: mass bin edges

--- SysUncEstimation/Acceptance/AccDiff_aMCNLO_FEWZ/ProdHist_MassWithinAcc.cxx
+++ SysUncEstimation/Acceptance/AccDiff_aMCNLO_FEWZ/ProdHist_MassWithinAcc.cxx
@@ -33,12 +33,11 @@ void ProdHist_MassWithinAcc( TString Sample = "aMCNLO", TString HLTname = "IsoMu
 
 	DYAnalyzer *analyzer = new DYAnalyzer( HLTname );
 
-	const Int_t nMassBin = 45;
 	Double_t MassBinEdges[nMassBin+1] = {15, 20, 25, 30, 35, 40, 45, 50, 55, 60,
 										 64, 68, 72, 76, 81, 86, 91, 96, 101, 106,
 										 110, 115, 120, 126, 133, 141, 150, 160, 171, 185,
 										 200, 220, 243, 273, 320, 380, 440, 510, 600, 700,
-										 830, 1000, 1200, 1500, 2000, 3000};
+										 830, 1000, 1500, 3000}; // -- high mass bins are merged -- //
 
 	TString BaseLocation = gSystem->Getenv("KP_DATA_PATH");
 
