7ae54f3b83d86429e265110a20dba8aa12f51186
Kyoengpil Lee <kplee@cern.ch>
2017-05-24 16:11:54 +0900

    results with electron acceptance

---
+++ TheoryValues/NLO_MRST2004qed_DYPI_PI/ElectronAcc_30_10/DYXSecNNLO_WithPI.cxx
@@ -0,0 +1,113 @@
+#include <Include/PlotTools.h>
+
+void SetError( TH1D* h_divide, TH1D* h_ratio );
+
+void DYXSecNNLO_WithPI()
+{
+	if( gSystem->mkdir( "./Local" ) == 0 )
+	printf("Directory [Local] is created\n");
+
+	gSystem->cd( "./Local" );
+
+	TString AnalyzerPath = gSystem->Getenv("KP_ANALYZER_PATH");
+	TString FileName_Ratio = AnalyzerPath + "/TheoryValues/NLO_MRST2004qed_DYPI_PI/ROOTFile_DYPI_DY_FEWZ.root";
+
+	TString ROOTFilePath = gSystem->Getenv("KP_ROOTFILE_PATH");
+	TString FileName_DYNNLO = ROOTFilePath + "/ROOTFile_xSec_Theory.root";
+
+	TH1D* h_DYNNLO = Get_Hist(FileName_DYNNLO, "h_DiffXsec_FEWZ_NNPDF_NNLO");
+	TH1D* h_ratio = Get_Hist(FileName_Ratio, "h_ratio");
+	TH1D* h_DYXSecNNLO_WithPI = (TH1D*)h_ratio->Clone("h_DYDiffXSecNNLO_WithPI");
+
+	Int_t nBin = h_DYNNLO->GetNbinsX();
+	for(Int_t i=0; i<nBin; i++)
+	{
+		Int_t i_bin = i+1;
+
+		Double_t NNLOXSec = h_DYNNLO->GetBinContent(i_bin);
+		Double_t RelErr_NNLOXSec = h_DYNNLO->GetBinError(i_bin) / NNLOXSec;
+
+		Double_t Ratio = h_ratio->GetBinContent(i_bin);
+		Double_t RelErr_ratio = h_ratio->GetBinError(i_bin) / Ratio;
+
+		Double_t NNLOXSec_DYPI = NNLOXSec * Ratio;
+		// -- error propagation -- //
+		Double_t RelErr_total = sqrt(RelErr_NNLOXSec*RelErr_NNLOXSec + RelErr_ratio*RelErr_ratio);
+
+		h_DYXSecNNLO_WithPI->SetBinContent(i_bin, NNLOXSec_DYPI);
+		h_DYXSecNNLO_WithPI->SetBinError(i_bin, RelErr_total * NNLOXSec_DYPI); // -- absolute error -- //
+	}
+
+	HistInfo *Hist_DYOnly = new HistInfo(kBlue, "DY (w/o PI)");
+	Hist_DYOnly->Set_Histogram( h_DYNNLO );
+	Hist_DYOnly->Set();
+
+	HistInfo *Hist_DYPI = new HistInfo(kRed, "DY (w/ PI)");
+	Hist_DYPI->Set_Histogram( h_DYXSecNNLO_WithPI );
+	Hist_DYPI->Set();
+	Hist_DYPI->CalcRatio_DEN( Hist_DYOnly->h );
+	SetError( Hist_DYPI->h_ratio, h_ratio );
+
+	TCanvas *c; TPad *TopPad; TPad *BottomPad;
+	SetCanvas_Ratio(c, "c_DiffXSec_DYOnly_DYPI", TopPad, BottomPad, 1, 1 );
+
+	// -- Top Pad -- //
+	c->cd();
+	TopPad->cd();
+	Hist_DYOnly->Draw("EPSAME");
+	Hist_DYPI->Draw("EPSAME");
+	SetHistFormat_TopPad( Hist_DYOnly->h, "d#sigma/dm [pb/GeV]" );
+
+	TLegend *legend;
+	SetLegend( legend );
+	Hist_DYOnly->AddToLegend( legend );
+	Hist_DYPI->AddToLegend( legend );
+	legend->Draw();
+
+	TLatex latex;
+	Latex_Simulation( latex );
+	latex.DrawLatexNDC(0.18, 0.38, "#font[42]{#scale[0.8]{DY: NNPDF 3.0, NNLO}}");
+	latex.DrawLatexNDC(0.18, 0.33, "#font[42]{#scale[0.8]{PI: MRST2004qed, NLO}}");
+
+	// -- Bottom Pad -- //
+	c->cd();
+	BottomPad->cd();
+
+	Hist_DYPI->h_ratio->SetMarkerColor(kBlack);
+	Hist_DYPI->h_ratio->SetLineColor(kBlack);
+	Hist_DYPI->DrawRatio("EPSAME");
+	SetHistFormat_BottomPad( Hist_DYPI->h_ratio, "m [GeV]", "(DY+PI)/DY", 0.95, 1.15 );
+	Hist_DYPI->h_ratio->GetYaxis()->SetNdivisions(504);
+
+	TF1 *f_line;
+	DrawLine( f_line );
+
+	c->SaveAs(".pdf");
+
+	TFile *f_output = TFile::Open("../ROOTFile_DYXSecNNLO_WithPI.root", "RECREATE");
+	f_output->cd();
+
+	h_DYNNLO->SetName("h_DYDiffXSecNNLO_WithoutPI");
+	h_DYNNLO->Write();
+	h_ratio->SetName("h_ratio_DYPI_PI");
+	h_ratio->Write();
+	h_DYXSecNNLO_WithPI->Write();
+}
+
+// -- correct the overestimated error of ratio -- //
+void SetError( TH1D* h_divide, TH1D* h_ratio )
+{
+	Int_t nBin = h_ratio->GetNbinsX();
+	for(Int_t i=0; i<nBin; i++)
+	{
+		Int_t i_bin = i+1;
+
+		Double_t divide = h_divide->GetBinContent(i_bin);
+		Double_t ratio = h_ratio->GetBinContent(i_bin);
+		if( divide != ratio )
+			printf("[%02d bin] (divide = %lf) != (ratio = %lf)\n", i_bin, divide, ratio);
+		
+		Double_t Error = h_ratio->GetBinError(i_bin);
+		h_divide->SetBinError(i_bin, Error);
+	}
+}
---
+++ TheoryValues/NLO_MRST2004qed_DYPI_PI/ElectronAcc_30_10/Results/ReadFEWZoutput.py
@@ -0,0 +1,131 @@
+Version = "20170524"
+
+from ROOT import TFile, TH1D
+from array import array
+
+def GetXsec( FileName ):
+	print FlieName
+	xSec = 0.0;
+	IntgErr = 0.0;
+	PDFErr_plus = 0.0;
+	PDFErr_minus = 0.0;
+	PDFErr = 0.0;
+
+	f = open(FileName, "r")
+	lines = f.readlines()
+
+	Next_PDFError = 0
+	for line in lines:
+		if "Sigma (pb)" in line:
+			items = line.split(" ")
+			for item in items:
+				if isNumber( item ):
+					xSec = float(item);
+					break;
+
+		elif "Integration Error (pb)" in line:
+			items = line.split(" ")
+			for item in items:
+				if isNumber( item ):
+					IntgErr = float(item);
+					break;
+
+		elif "PDF Error (pb)" in line:
+			items = line.split(" ")
+			for item in items:
+				if isNumber( item ):
+					PDFErr_plus = float(item)
+					Next_PDFError = 1
+					break;
+
+		elif Next_PDFError == 1:
+			items = line.split(" ")
+			for item in items:
+				if isNumber( item ):
+					PDFErr_minus = float(item)
+					Next_PDFError = 0
+					break;
+
+
+	print "x-section = %lf, integration error = %lf, PDFError(+,-) = (%lf, %lf)\n" % ( xSec, IntgErr, PDFErr_plus, PDFErr_minus )
+
+	if PDFErr_plus > PDFErr_minus:
+		PDFErr = PDFErr_plus
+	else:
+		PDFErr = PDFErr_minus
+
+	return [xSec, IntgErr, PDFErr]
+
+def isNumber(s):
+	try:
+		float(s)
+		return True
+	except ValueError:
+		return False
+
+def MakeHistogram( process, values ):
+	nMassBin = 43
+	MassBinEdges = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60,
+				 64, 68, 72, 76, 81, 86, 91, 96, 101, 106,
+				 110, 115, 120, 126, 133, 141, 150, 160, 171, 185,
+				 200, 220, 243, 273, 320, 380, 440, 510, 600, 700,
+				 830, 1000, 1500, 3000]
+
+	h = TH1D("h_"+process, "", nMassBin, array("d", MassBinEdges) )
+
+	i = 0
+	for value in values:
+		i_bin = i+1;
+
+		h.SetBinContent(i_bin, value[0])
+		h.SetBinError(i_bin, value[1]) # -- PDF error: will be cancelled when (DY+PI)/DY ratio calcalation -- #
+
+		i = i+1;
+
+	return h
+
+# -- main part -- #
+nMassBin = 43
+MassBinEdges = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60,
+			 64, 68, 72, 76, 81, 86, 91, 96, 101, 106,
+			 110, 115, 120, 126, 133, 141, 150, 160, 171, 185,
+			 200, 220, 243, 273, 320, 380, 440, 510, 600, 700,
+			 830, 1000, 1500, 3000]
+
+values_DY = []
+values_DYPI = []
+
+for i in range(0, nMassBin):
+	LowerEdge = MassBinEdges[i]
+	UpperEdge = MassBinEdges[i+1]
+
+	MassRange = "M%.0fto%.0f" % (LowerEdge, UpperEdge)
+
+	Process = "DY"
+	FlieName = "NLO.v"+Version+"_"+MassRange+"_"+Process+"_NLO_MRST2004qed.dat"
+	value_DY = GetXsec( FlieName )
+	values_DY.append( value_DY )
+
+	Process = "DYPI"
+	FlieName = "NLO.v"+Version+"_"+MassRange+"_"+Process+"_NLO_MRST2004qed.dat"
+	value_DYPI = GetXsec( FlieName )
+	values_DYPI.append( value_DYPI )
+
+h_DY = MakeHistogram( "DY", values_DY )
+h_DYPI = MakeHistogram( "DYPI", values_DYPI )
+
+h_DY.Sumw2()
+h_DYPI.Sumw2()
+
+h_ratio = h_DY.Clone()
+h_ratio.SetName("h_ratio")
+h_ratio.Divide( h_DYPI, h_DY )
+
+f_output = TFile("ROOTFile_DYPI_DY_FEWZ.root", "RECREATE")
+
+f_output.cd()
+h_DY.Write()
+h_DYPI.Write()
+h_ratio.Write()
+
+f_output.Close()
