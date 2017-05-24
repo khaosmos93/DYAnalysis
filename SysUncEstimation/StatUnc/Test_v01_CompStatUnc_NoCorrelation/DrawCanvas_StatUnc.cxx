4f1fd2f871a2c72ba77cd4fa36c187b14ddeed59
Kyoengpil Lee <kplee@cern.ch>
2017-05-24 13:07:02 +0900

    draw comparison plot of stat.unc

--- SysUncEstimation/StatUnc/Test_v01_CompStatUnc_NoCorrelation/DrawCanvas_StatUnc.cxx
+++ SysUncEstimation/StatUnc/Test_v01_CompStatUnc_NoCorrelation/DrawCanvas_StatUnc.cxx
@@ -6,29 +6,32 @@ void DrawCanvas_StatUnc()
 		printf("Directory [Local] is created\n");
 	gSystem->cd( "./Local" );
 
-	TH1D* h_WithCorr = Get_Hist("../ROOTFile_RelStatUnc.root", "h_RelStatUnc_Percent");
-	TH1D* h_WithCorr_FpoF = Get_Hist("../ROOTFile_RelStatUnc.root", "h_FpoF_RelStatUnc_Percent");
+	TString AnalyzerPath = gSystem->Getenv("KP_ANALYZER_PATH");
+	TH1D* h_WithCorr = Get_Hist(AnalyzerPath + "/SysUncEstimation/StatUnc/ROOTFile_RelStatUnc.root", "h_RelStatUnc_Percent");
+	TH1D* h_WithCorr_FpoF = Get_Hist(AnalyzerPath + "/SysUncEstimation/StatUnc/ROOTFile_RelStatUnc.root", "h_FpoF_RelStatUnc_Percent");
 
 	TString ROOTFilePath = gSystem->Getenv("KP_ROOTFILE_PATH");
 	TH1D* h_NoCorr = Get_Hist(ROOTFilePath+"/ROOTFile_Results_DYAnalysis_76X.root", "h_RelUnc_Stat_7TeVMethod");
 	h_NoCorr->Scale( 100 ); // -- convert to % -- //
 
-	HistInfo *Hist_WithCorr = new HistInfo( kBlue, "Randomized yield" );
-	Hist_WithCorr->Set_Histogram( h_WithCorr );
-	Hist_WithCorr->Set();
-
-	HistInfo *Hist_WithCorr_FpoF = new HistInfo( kGreen+2, "Randomized yield, post-FSR" );
-	Hist_WithCorr_FpoF->Set_Histogram( h_WithCorr_FpoF );
-	Hist_WithCorr_FpoF->Set();
-
 	HistInfo *Hist_NoCorr = new HistInfo( kRed, "7 TeV method" );
 	Hist_NoCorr->Set_Histogram( h_NoCorr );
 	Hist_NoCorr->Set();
 
+	HistInfo *Hist_WithCorr = new HistInfo( kBlue, "Randomized yield" );
+	Hist_WithCorr->Set_Histogram( h_WithCorr );
+	Hist_WithCorr->Set();
+	Hist_WithCorr->CalcRatio_DEN( Hist_NoCorr->h );
+
+	HistInfo *Hist_WithCorr_FpoF = new HistInfo( kGreen+2, "Randomized yield, post-FSR" );
+	Hist_WithCorr_FpoF->Set_Histogram( h_WithCorr_FpoF );
+	Hist_WithCorr_FpoF->Set();
+	Hist_WithCorr_FpoF->CalcRatio_DEN( Hist_NoCorr->h );
+
 	TString CanvasName = "c_Comp_RelStatUnc";
 	TCanvas *c; TPad *TopPad; TPad *BottomPad;
 
-	SetCanvas_Ratio( c, CanvasName, TopPad, BottomPad, 1, 0 );
+	SetCanvas_Ratio( c, CanvasName, TopPad, BottomPad, 1, 1 );
 	c->cd();
 	TopPad->cd();
 
@@ -36,7 +39,28 @@ void DrawCanvas_StatUnc()
 	Hist_WithCorr_FpoF->Draw("HISTLPSAME");
 	Hist_WithCorr->Draw("HISTLPSAME");
 
-	SetHistFormat_TopPad( Hist_NoCorr->h, "Stat. Unc. (%)" );
+	SetHistFormat_TopPad( Hist_NoCorr->h, "Stat. Unc. (%)");
 
+	TLegend *legend;
+	SetLegend( legend, 0.17, 0.72, 0.60, 0.95);
+	Hist_NoCorr->AddToLegend( legend );
+	Hist_WithCorr_FpoF->AddToLegend( legend );
+	Hist_WithCorr->AddToLegend( legend );
+	legend->Draw();
 
+	TLatex latex;
+	Latex_Preliminary( latex, 2.8, 13 );
+
+	c->cd();
+	BottomPad->cd();
+
+	Hist_WithCorr->DrawRatio( "HISTLPSAME" );
+	Hist_WithCorr_FpoF->DrawRatio( "HISTLPSAME" );
+
+	SetHistFormat_BottomPad( Hist_WithCorr->h_ratio, "m [GeV]", "rndm.yield/7TeV", 0.6, 2.0 );
+
+	TF1 *f_line;
+	DrawLine( f_line );
+
+	c->SaveAs(".pdf");
 }
