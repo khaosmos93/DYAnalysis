41d24433404a751c11c04019400412bad15093d3
Kyoengpil Lee <kplee@cern.ch>
2017-06-27 11:03:10 +0900

    code for drawing all official-style plots

--- OfficialStyle/script_DrawAllOfficialPlots.sh
+++ OfficialStyle/script_DrawAllOfficialPlots.sh
@@ -4,12 +4,16 @@ root -b -q dimuonMass.C
 root -b -q DrawRelUnc_Summary_LogY.cxx
 
 # -- comparison with various PDF above 200 GeV -- #
-root -b -q Comparison_VariousPDF/InputFileMaker.cxx
-root -b -q 'Comparison_VariousPDF/Comparison_VariousPDF.cxx("MM")'
-root -b -q 'Comparison_VariousPDF/Comparison_VariousPDF.cxx("EE")'
-root -b -q 'Comparison_VariousPDF/Comparison_VariousPDF.cxx("LL")'
+
+cd Comparison_VariousPDF
+
+root -b -q InputFileMaker.cxx
+root -b -q 'Comparison_VariousPDF.cxx("MM")'
+root -b -q 'Comparison_VariousPDF.cxx("EE")'
+root -b -q 'Comparison_VariousPDF.cxx("LL")'
 
 # -- comparison with DY+PI -- #
-root -b -q Comparison_DYPI_PI/Ratio_TheoryToData.cxx
+cd ../Comparison_DYPI_PI
+root -b -q Ratio_TheoryToData.cxx
 
 echo "finished"
